51724f3e0edcd083e0ae08c3bfcc95a046ea88f2
Michael Wisely <michaelwisely@gmail.com>
2014-10-01 16:20:52 -0500

    Change location of sqlite db

--- discuss/discuss/settings.py
+++ discuss/discuss/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'discuss.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(VAR_DIR, 'db', 'db.sqlite3'),
     }
 }
 
