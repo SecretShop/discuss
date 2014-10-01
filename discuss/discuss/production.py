3af26b33ea70170a3b7ba39aea5a2f9f3d48a81e
Michael Wisely <michaelwisely@gmail.com>
2014-10-01 16:32:36 -0500

    Disable debug mode for production

--- discuss/discuss/production.py
+++ discuss/discuss/production.py
@@ -1,8 +1,5 @@
 from discuss.discuss.settings import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ##########################################################################
 #
